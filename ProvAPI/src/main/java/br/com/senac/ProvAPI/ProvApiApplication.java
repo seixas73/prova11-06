d20751dda3ea82a832742d7b48ec8d36c426e77e
Artur Seixas <oseixas73@gmail.com>
2026-06-11 19:14:27 -0300

    add

---
+++ ProvAPI/src/main/java/br/com/senac/ProvAPI/ProvApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.senac.ProvAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvApiApplication.class, args);
+	}
+
+}
---
+++ ProvAPI/src/test/java/br/com/senac/ProvAPI/ProvApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senac.ProvAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
